e2709f7c62bb6d389305294fbb6be03edbfc89d4
Sahand <sahandevs@gmail.com>
2019-06-22 11:59:40 +0430

    implement screen capture

--- lib/shake_to_report.dart
+++ lib/shake_to_report.dart
@@ -1,7 +1,49 @@
 library shake_to_report;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:ui';
+
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shake_event/shake_event.dart';
+
+class ShakeToReport extends StatefulWidget {
+  final Widget child;
+
+  const ShakeToReport({Key key, this.child}) : super(key: key);
+
+  @override
+  _ShakeToReportState createState() => _ShakeToReportState();
+}
+
+class _ShakeToReportState extends State<ShakeToReport> with ShakeHandler {
+  var _applicationKey = new GlobalKey();
+
+  @override
+  void dispose() {
+    resetShakeListeners();
+    super.dispose();
+  }
+
+  @override
+  shakeEventListener() {
+    takeScreenshot();
+    return super.shakeEventListener();
+  }
+
+  takeScreenshot() async {
+    RenderRepaintBoundary boundary =
+        _applicationKey.currentContext.findRenderObject();
+    var image = await boundary.toImage();
+    var byteData = await image.toByteData(format: ImageByteFormat.png);
+    var pngBytes = byteData.buffer.asUint8List();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    startListeningShake(20);
+    return RepaintBoundary(
+      key: _applicationKey,
+      child: widget.child,
+    );
+  }
 }
